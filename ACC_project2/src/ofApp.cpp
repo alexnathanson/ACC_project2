08169e87af85cd3548eeff2a6ccca3f3babfe361
alexnathanson <alex@alexnathanson.com>
2018-02-10 11:47:28 -0500

    added open CV example

--- ACC_project2/src/ofApp.cpp
+++ ACC_project2/src/ofApp.cpp
@@ -1,72 +1,153 @@
 #include "ofApp.h"
 
+//ACC Project 2
+//Group members: Alex Nathanson & Kevin Li
+
 //--------------------------------------------------------------
-void ofApp::setup(){
-	//test!
-	//another git test!
+void ofApp::setup() {
+
+	/*
+	vidPlayer.load("fingers.mov");
+	vidPlayer.play();
+	vidPlayer.setLoopState(OF_LOOP_NORMAL); // a number of different loop types
+	*/
+
+	vidGrabber.setVerbose(true);
+	vidGrabber.setDeviceID(0);
+	vidGrabber.initGrabber(320, 240);
+	//vidGrabber.videoSettings();
+
+	//must allocate pixels before reading anything in to them
+	colorImg.allocate(320, 240);
+	grayImage.allocate(320, 240);
+	grayBg.allocate(320, 240);
+	grayDiff.allocate(320, 240);
+
+	bLearnBakground = true;
+	threshold = 10;
+
+	//makes it so it doesn't clear the background automatically
+	ofSetBackgroundAuto(false);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+	//ofBackground(100,100,100);
+
+	bool bNewFrame = false;
+
+	vidGrabber.update();
+
+	//vidPlayer.update();
+	bNewFrame = vidGrabber.isFrameNew();
+
+	if (bNewFrame) {
+
+
+		colorImg.setFromPixels(vidGrabber.getPixels());
+
+		grayImage = colorImg;
+		if (bLearnBakground == true) {
+			grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
+			bLearnBakground = false;
+		}
+
+		// take the abs value of the difference between background and incoming and then threshold:
+		grayDiff.absDiff(grayBg, grayImage);
+		grayDiff.threshold(threshold);
+
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		contourFinder.findContours(grayDiff, 20, (340 * 240) / 3, 10, true);	// find holes
+	}
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+
+	// draw the incoming video image
+	ofSetHexColor(0xffffff);
+	ofPushMatrix();
+	ofTranslate(ofGetWidth() / 2 - colorImg.getWidth() / 2, ofGetHeight() / 2 - colorImg.getHeight() / 2);
+	if (bShowVideo) {
+		colorImg.draw(0, 0);
+	}
+	for (int i = 0; i < contourFinder.nBlobs; i++) {
+		ofSetColor(ofColor::fuchsia);
+		ofFill();
+		ofDrawEllipse(contourFinder.blobs[i].boundingRect.getCenter(), 20, 20);
+	}
+	ofPopMatrix();
+
+	ofDrawBitmapString("Press 'v' to toggle video and path drawing", ofGetWidth() / 2.0, ofGetHeight() - 100.0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+	switch (key) {
+	case 'v':
+		ofBackground(ofColor::white);
+		bShowVideo = !bShowVideo;
+		break;
+	case '+':
+		threshold++;
+		if (threshold > 255) threshold = 255;
+		break;
+	case '-':
+		threshold--;
+		if (threshold < 0) threshold = 0;
+		break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- ACC_project2/src/ofApp.h
+++ ACC_project2/src/ofApp.h
@@ -2,23 +2,48 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+#include "ofxOpenCv.h"
 
-	public:
-		void setup();
-		void update();
-		void draw();
+#define _USE_LIVE_VIDEO		// uncomment this to use a live camera
+// otherwise, we'll use a movie file
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+#ifdef _USE_LIVE_VIDEO
+	ofVideoGrabber 		vidGrabber;
+#else
+	ofVideoPlayer 		vidPlayer;
+#endif
+
+	ofxCvColorImage			colorImg;
+
+	ofxCvGrayscaleImage 	grayImage;
+	ofxCvGrayscaleImage 	grayBg;
+	ofxCvGrayscaleImage 	grayDiff;
+
+	ofxCvContourFinder 	contourFinder;
+
+	int 				threshold;
+	bool				bLearnBakground;
+	bool				bShowVideo = true;
+
+
 };
+
